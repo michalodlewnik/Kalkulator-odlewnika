d645c7aad7797c18a1658f9d55a7b39ac64401f1
michalodlewnik <michalwnuk90@gmail.com>
2026-01-22 07:10:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ st.markdown("""
     <style>
 
       /* 1. Usunięcie marginesów na samej górze strony */
-    .block-container { padding-top: 5rem !important; padding-bottom: 5rem !important; }
+    .block-container { padding-top: 3rem !important; padding-bottom: 5rem !important; }
     
     /* 2. ZMNIEJSZENIE PRZERW MIĘDZY WIERSZAMI */
     div[data-testid="stVerticalBlock"] > div {
@@ -123,6 +123,7 @@ st.markdown(f"""
         <div style="font-size: 40px; font-weight: 800;">{total_si_inc:.2f} %</div>
     </div>
     """, unsafe_allow_html=True)
+
 
 
 
